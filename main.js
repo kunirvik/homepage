5dfab287babb9f65e8c4d9a875f0fa21c5fa9402
kunirvik <123270547+kunirvik@users.noreply.github.com>
2024-04-01 10:14:03 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import './style.css'
+
+import { gsap } from 'gsap/gsap-core';
+import { ScrollTrigger } from 'gsap/all';  // Correct import for ScrollTrigger
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// Регистрируем плагин ScrollToPlugin
+const container = document.querySelector('.layout');
+const slides = document.querySelectorAll('.slide');
+const containerWidth = container.offsetWidth;
+gsap.to( slides, {
+	xPercent: -110 * (slides.length - 1),
+	ease: "none", 
+	scrollTrigger: {
+		markers:true,
+		trigger: container, 
+		pin: true, 
+		scrub: 2, 
+		start:'top 20',
+		
+		end: () => '+=' + containerWidth, 
+		anticipatePin: 1, 
+		invalidateOnRefresh: true, 
+	}
+	
+});
